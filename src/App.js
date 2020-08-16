d22c7d0ec6fe70e2909aee70d839786d98b18b32
pacis32 <pacis32@gmail.com>
2020-08-16 11:39:11 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import './App.css';
 function App() {
   return (
     <div >
-<<<<<<< HEAD
      <h2>Hello Shoppers</h2>
-=======
-     <h2>Hello shoppers</h2>
-     
->>>>>>> 3efa374aeae4bbbd34f60a899f8e8da69f173905
     </div>
   );
 }
