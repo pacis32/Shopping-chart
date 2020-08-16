8618d1ce83a95ec55018be95054706980faeb759
pacis32 <pacis32@gmail.com>
2020-08-16 13:18:24 -0500

    components rendering in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-
+import TshirtList from './TshirtList'
 import './App.css';
+import Navibar from './Components/Navbar'
+import Cart from './Components/Cart'
+import Default from './Components/Default'
+import Details from './Components/Details'
+import Product from './Components/Product'
+import ProductList from './Components/ProductList'
+
+
 
 function App() {
   return (
+    <React.Fragment>
     <div >
-     <h2>Hello Shoppers</h2>
-     <TshirtList/>
+      <h2>Hello Shoppers</h2>
+      <Navibar/>
+      <ProductList/>
+      <Cart/>
+      <Details/>
+      <Default/>
     </div>
+    </React.Fragment>
+    
   );
 }
 
