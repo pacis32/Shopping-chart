e4a7429dccdfd12e78fa56304a3af44efb014362
pacis32 <pacis32@gmail.com>
2020-08-17 10:40:51 -0500

    updating cart.js

---
+++ src/containers/cart.js
@@ -0,0 +1,36 @@
+import React, { Component ,Fragment} from 'react'
+import CartComponent from '../Components/Cart'
+import {fetchCartList,deleteCartItem} from '../actions/cart'
+import {connect} from 'react-redux'
+
+  class Cart extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+        fetchCartList("https://www.scorebat.com/video-api/v1/",this.props.dispatch);
+    }
+
+    componentDidUpdate(){
+        console.log("Update")
+    }
+
+    render() {
+        return (
+           <Fragment>
+               {/* pass cart data in cart componet through props */}
+               <CartComponent cartList={this.props.cartList} dispatch={this.props.dispatch} isLoggedIn={this.props.appState && this.props.appState.isLoggedIn || false}/>
+           </Fragment>
+        )
+    }
+}
+//connect store and container and makes state available in container's props
+//disptach comes from store, to update reducer with action
+const mapPropsToState = (state)=>({
+        cartList:state.cart && state.cart.data ||[],
+        home:state.home ||[],
+        appState:state.appState || []
+})
+
+export default connect(mapPropsToState)(Cart)
