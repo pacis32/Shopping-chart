5d7462c124b73250fa35288b2c553c32b9bfc749
pacis32 <pacis32@gmail.com>
2020-08-17 10:36:01 -0500

    updating App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import TshirtList from './TshirtList'
+
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import rootReducer from './reducers'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
 import './App.css';
 import Navibar from './Components/Navbar'
-import Cart from './Components/Cart'
+import Cart from './containers/cart'
 import Default from './Components/Default'
 import Details from './Components/Details'
-import Product from './Components/Product'
 import ProductList from './Components/ProductList'
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+  //
+)
 
-function App() {
-  return (
-    <React.Fragment>
-    <div >
-      <h2>Hello Shoppers</h2>
+
+const App =()=> {
+
+
+    return (
+      <React.Fragment>
+      <Provider store={store}>
       <Navibar/>
-      <ProductList/>
-      <Cart/>
-      <Details/>
-      <Default/>
-    </div>
-    </React.Fragment>
-    
-  );
-}
+        <Switch>
+          <Route exact path="/" component={ProductList}/> 
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+        </Provider> 
+      </React.Fragment>
+      
+    );
+  }
+  
+
 
 export default App;
