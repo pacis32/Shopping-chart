46f725f799465eecc6f48b7bce8f621cf21c5b6d
pacis32 <pacis32@gmail.com>
2020-08-19 02:11:55 -0500

    modifying

--- src/constants/actions.js
+++
@@ -1,9 +0,0 @@
-export const cartActions = {
-        "fetchCartData":"FETCH_CART_DATA" ,
-        "deleteCartItem":"DELETE_CART_ITEM"
-}
-
-export const homeActions = {
-    "fetchAllProducts":'FETCH_ALL_PRODUCTS',
-    "fasfsa":'fasfasf'
-}
