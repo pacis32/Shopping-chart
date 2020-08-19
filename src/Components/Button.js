ce3e65f09320aa2bbd9807c85173d994d8a5b5a3
pacis32 <pacis32@gmail.com>
2020-08-19 02:10:03 -0500

    adding component

---
+++ src/Components/Button.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.button`
+  text-transform:capitalized;
+  font-size: 1.4rem;
+  background: transform;
+  border: 0.05erm solid var(--lightBlue);
+  color: var(--lightBlue);
+  padding:0.2rem 0.5rem;
+  cursor:pointer;
+  margin:0.2erm 0.5rem 0.2rem 0;
+  transition:all 0.5s ease-in-out
+  &:hover{
+    background:var(--lightBlue);
+    color: var(--mainBlue);
+  }
+  &:focus{
+    outline:none:
+    
+  }
+  
+  `
+  
