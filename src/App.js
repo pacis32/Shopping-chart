ea0de057b2c61681c72fe0f8c169bd5932c6775c
pacis32 <pacis32@gmail.com>
2020-08-19 17:32:09 -0500

    stlying the navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-
-
-
-    import React from "react";
-    import {
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import React from "react";
+import {
       BrowserRouter as Router,
       Switch,
       Route,
       Link
     } from "react-router-dom";
-    import Products from './Components/Products'
-    import store from './store'
-    import { Provider } from "react-redux";
+import Products from './Components/Products'
+import store from './store'
+import { Provider } from "react-redux";
 import Navbar from "./Components/Navbar";
 import Basket from './Components/Basket'
+
 
     
    
