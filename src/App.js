5527dc9f5524cbfbd2be343bf332f47809c9d4d2
pacis32 <pacis32@gmail.com>
2020-08-19 02:08:10 -0500

    modifying app.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
-import React from 'react';
 
-import {Provider} from 'react-redux'
-import {createStore} from 'redux'
-import rootReducer from './reducers'
-import {composeWithDevTools} from 'redux-devtools-extension'
 
+import React, { Component } from 'react'
 import './App.css';
 import Navibar from './Components/Navbar'
-import Cart from './containers/cart'
+import Product from './Components/Products'
+import Cart from './Components/Cart'
 import Default from './Components/Default'
 import Details from './Components/Details'
 import ProductList from './Components/ProductList'
+import Products from './Components/Products'
 import { Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+export default class App extends Component  {
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools()
-  //
-)
+  constructor(props) {
+    super(props);
+    this.state = {
+        products: [], filteredProducts: []
 
+    }
 
-const App =()=> {
+}
+componentWillMount(){
+  fetch(' http://localhost:8000/products')
+      .then(res => JSON)
 
-
+      .then(data => this.setState({
+          products: data,
+          filteredProducts: data
+      }));
+    }
+  render(){
     return (
       <React.Fragment>
-      <Provider store={store}>
+     
       <Navibar/>
         <Switch>
-          <Route exact path="/" component={ProductList}/> 
+          <Route exact path="/" component={Products}/>
           <Route path="/details" component={Details}/>
           <Route path="/cart" component={Cart}/>
           <Route component={Default}/>
         </Switch>
-        </Provider> 
-       <div className='container'>
-         <div className='row'>
-           <div className='col-6'>column num one</div>
-           <div className='col-6'>
-             <span>
-               <i className="fas fa-home"/>
-             </span>
-           </div>
-         </div>
-       </div>
-
-      </React.Fragment>
+        </React.Fragment>
+       
+       
       
     );
   }
+  }
+    
   
-
-
-export default App;
